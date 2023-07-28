084d46f0255c27036822cd9e8217d3210e1e163f
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-28 11:27:54 -0500

    playing around INT's

---
+++ src/FavNumber.java
@@ -0,0 +1,15 @@
+public class FavNumber {
+    public static void main(String[] args) {
+        // Create an int variable named myFavoriteNumber and assign your favorite number to it
+        int x = 5;
+        int y = 20;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x += 60);
+        System.out.println(x - y);
+        System.out.println(y + x);
+
+        // Print it out to the console
+        System.out.println("My favorite number is: " + x);
+    }
+}
