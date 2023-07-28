1dec3805af2090cafdf8449770f74a7a634e47f8
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-28 11:11:14 -0500

    playing around with Special characters

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Kurt!");
-        System.out.println("Hello, Steve!");
+        System.out.println("Hello, Steve!\n");
         System.out.print("This is on the,");
-        System.out.print(" Same line!");
+        System.out.print(" Same line!\n");
+        System.out.println("I tabbed this one\t");
+
+        System.out.println("I am a string!");
+        System.out.println("6");
+
+
 
 
     }
