f3466409de3b68bc55bfd9878e1985ec28124ee6
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-28 11:03:39 -0500

    ran through some exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kurt!");
+        System.out.println("Hello, Steve!");
+        System.out.print("This is on the,");
+        System.out.print(" Same line!");
+
+
+    }
+
+}
