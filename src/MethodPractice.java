0e04fe1cc03255af4eb7d8f46180145c06274486
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-02 21:36:22 -0500

    completed mini exercises from Lecture for homework

--- src/MethodPractice.java
+++ src/MethodPractice.java
@@ -1,27 +1,77 @@
+
+//    TODO: Write a method, returnName, that returns the string of your name. DONE
+//    TODO: Write a method, returnLongerString, that takes in two strings and returns the longer of the two strings.DONE
+//    TODO: Write a method, canBeHalved, that takes in an integer and returns true if the input integer is evenly divisible by two.DONE
+//    TODO: !! Complete math exercises from the curriculum !! DONE
+
+
+import java.util.Scanner;
+
 public class MethodPractice {
 
+//    public static String returnName() {
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.nextLine();
+//        scanner.close();
+//        String trimmedInput = userInput.trim();
+//        return trimmedInput;
+//    }
 
-    //    TODO: Write a method, returnName, that returns the string of your name.
-    //    TODO: Write a method, returnLongerString, that takes in two strings and returns the longer of the two strings.
-    //    TODO: Write a method, canBeHalved, that takes in an integer and returns true if the input integer is evenly divisible by two.
-    //    TODO: !! Complete math exercises from the curriculum !!
+    public static String returnLongerString() {
 
-
-    public static void returnName(){
-// takes in string
-
-
-// stores string to var called name
-
-
-// then return name
+        Scanner scanner = new Scanner(System.in);
+        // take in 2 strings
+        System.out.println("Enter the first string:");
+        String str1 = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String str2 = scanner.nextLine();
+        // return longest of strings
+        if (str1.length() > str2.length()) {
+            return str1;
+        } else if (str2.length() > str1.length()) {
+            return str2;
+        } else {
+            return "Both strings are of equal length.";
+        }
     }
 
 
+    public static boolean canBeHalved(int userInput) {
+        // check if the input number is divisible by 2
+        if (userInput % 2 == 0) {
+            int halvedNumber = userInput / 2;
+            System.out.println("Half of " + userInput + " is: " + halvedNumber + " , so your number can be halved.");
+            return true;
+        } else {
+            System.out.println("Your number cannot be halved.");
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
 
-        returnName();
+        // Declare a var to returned value by calling Method Name
+
+//        String name = returnName();
+//        System.out.println("Your name is : " + name);
+
+        // Declare a var to returned value by calling Method Name
+//        String longerString = returnLongerString();
+//        System.out.println(longerString);
+
+
+
+        // get user number
+        System.out.println("Enter a number you would like to halve:");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        scanner.close();
+
+        // Call the method and store the result in a variable
+        boolean isHalved = canBeHalved(userInput);
+        System.out.println(isHalved);
 
     }
+
 }
+
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,83 @@
+//Basic Arithmetic
+//
+//Create four separate methods. Each will perform an arithmetic operation:
+//
+//Addition
+//Subtraction
+//Multiplication
+//Division
+//Each function needs to take two parameters/arguments so that the operation can be performed.
+//
+//Test your methods and verify the output.
+//
+//Add a modulus method that finds the modulus of two numbers.
+//
+//Food for thought: What happens if we try to divide by zero? What should happen?
+
+import java.util.Scanner;
 
 public class MethodsExercises {
+    /////METHODS
+    public static int Addition() {
+        Scanner scanner = new Scanner(System.in);
+        int numOne = scanner.nextInt();
+        int numTwo = scanner.nextInt();
+        int addedSum = numOne + numTwo;
+        return addedSum;
+    }
 
+
+    public static int Subtraction() {
+        Scanner scanner = new Scanner(System.in);
+        int numOne = scanner.nextInt();
+        int numTwo = scanner.nextInt();
+        int subtractedSum = numOne - numTwo;
+        return subtractedSum;
+    }
+
+    public static int Multiplication() {
+        Scanner scanner = new Scanner(System.in);
+        int numOne = scanner.nextInt();
+        int numTwo = scanner.nextInt();
+        int multipliedSum = numOne * numTwo;
+        return multipliedSum;
+    }
+
+
+    public static String Division() {
+        Scanner scanner = new Scanner(System.in);
+        int numOne = scanner.nextInt();
+        int numTwo = scanner.nextInt();
+
+        if (numTwo == 0) {
+            return "Zero Division error : Program Crash Prevented ";
+        } else {
+            int dividedSum = numOne / numTwo;
+            // get sum in String form
+            return String.valueOf(dividedSum);
+        }
+    }
+
+    ;
+
+    ////// MAIN
     public static void main(String[] args) {
+        System.out.println("Enter Two Numbers");
+        int addedNumbers = Addition();
+        System.out.println(addedNumbers);
 
 
+        System.out.println("Enter Two Numbers");
+        int subtractedNumbers = Subtraction();
+        System.out.println(subtractedNumbers);
 
+        System.out.println("Enter Two Numbers");
+        int multipliedSumOfNumbers = Multiplication();
+        System.out.println(multipliedSumOfNumbers);
+
+        System.out.println("Enter Two Numbers");
+        String dividedNumbers = Division();
+        System.out.println(dividedNumbers);
 
 
     }
