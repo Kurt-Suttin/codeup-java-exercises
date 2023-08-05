dad824c51b3e20a2690fcaf042a58d0ad1ff41c5
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-05 14:55:29 -0500

    added comments on Circle class

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,21 +1,25 @@
 package shapes;
 
 public class Circle {
-    /// INSTANCES
+    // Private field to store the radius of the circle
     private double radius;
+
+    // Private fields to store the calculated area and circumference
     private double area;
     private double circumference;
-// CONSTRUCTOR
+
+    // Constructor to initialize the radius of the circle
     public Circle(double radius) {
         this.radius = radius;
-
     }
-// GETTERS
+
+    // Method to calculate and return the area of the circle
     public double getArea() {
         this.area = 3.14159265359 * Math.pow(radius, 2);
         return area;
     }
 
+    // Method to calculate and return the circumference of the circle
     public double getCircumference() {
         this.circumference = 2 * 3.14159265359 * radius;
         return circumference;
