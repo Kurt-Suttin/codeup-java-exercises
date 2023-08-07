5b8c4737fa87bcf5713962fe99591e5b8d3d7a28
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-06 21:21:16 -0500

    added sout the new circle instance so i can see the address

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,21 +2,13 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-     Circle circle = new Circle(6);
-     circle.getArea();
-     circle.getCircumference();
-        System.out.println("your circumference is : " + circle.getCircumference());
-        System.out.println("your area is : " + circle.getArea());
-
-
+        Circle grandpa = new Circle(6);
+        grandpa.getArea();
+        grandpa.getCircumference();
+        System.out.println("your circumference is : " + grandpa.getCircumference());
+        System.out.println("your area is : " + grandpa.getArea());
+        System.out.println(grandpa);
     }
-
-
-
-
-
-
-
 
 
 }
