c67e23a91f63ad933be4686d1b627691eedb4d83
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-07 18:17:51 -0500

    finished square class and override exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,12 +2,40 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Circle newCircle = new Circle(6);
-        newCircle.getArea();
-        newCircle.getCircumference();
-        System.out.println("your circumference is : " + newCircle.getCircumference());
-        System.out.println("your area is : " + newCircle.getArea());
-        System.out.println(newCircle);
+//        Circle newCircle = new Circle(6);
+//        newCircle.getArea();
+//        newCircle.getCircumference();
+//        System.out.println("your circumference is : " + newCircle.getCircumference());
+//        System.out.println("your area is : " + newCircle.getArea());
+//        System.out.println(newCircle);
+
+//create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class
+// with a width of 4 and a length of 5
+        Rectangle box1 = new Rectangle(4,5);
+//        verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+        System.out.println("box1's perimeter is " + box1.getPerimeter()+" respectively");
+        System.out.println( "box1's area is " + box1.getArea()+" respectively");
+///
+
+//    create a variable of the type Rectangle named box2 and assign it a new instance of the Square class that has a side value of 5
+//
+ Rectangle box2 = new Square(5);
+
+//verify that the getPerimeter and getArea methods return 20 and 25, respectively.
+        System.out.println("box2's perimeter is " + box2.getPerimeter()+ " respectively");
+        System.out.println( "box2's area is " + box2.getArea() +" respectively");
+
+//In the Square class, override the getArea and getPerimeter methods
+// with the following definitions for a square
+// perimeter = 4 x side
+// area = side ^ 2 (aka second power)
+
+
+        Square square = new Square(5);
+        System.out.println("Square Area: " + square.getArea()); // Calls Square's overridden getArea
+        System.out.println("Square Perimeter: " + square.getPerimeter()); // Calls Square's overridden getPerimeter
+
+
     }
 
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,6 +1,33 @@
 package shapes;
+//Create a class named Square, also inside of shapes, that extends Rectangle.
+// Square should define a constructor that accepts one argument, side, and calls the parent's constructor to set both the length and width to the value of side.
 
-public class Square {
+public class Square extends Rectangle{
+
+///In the Square class, override the getArea and getPerimeter methods
+    //// with the following definitions for a square
+    public Square(int side) {
+        super(side,side);
+    }
+
+    @Override
+    public int getArea() {
+        return width * width; // Area of a square is side * side
+    }
+
+    //// perimeter = 4 x side
+    @Override
+    public int getPerimeter() {
+        return 4 * width; // Perimeter of a square is 4 times the side
+    }
+
+    //// area = side ^ 2 (aka second power)
+
+    //Test your code by creating a new class, ShapesTest (also inside of shapes) with a main method. Inside the main method,
+
+//
+
+
 
 
 
