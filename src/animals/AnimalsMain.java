9d3aa31b8d772a0da17853825e158f0cb19bc442
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-18 18:45:02 -0500

    started animal personal class

---
+++ src/animals/AnimalsMain.java
@@ -0,0 +1,8 @@
+package animals;
+
+public class AnimalsMain {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        rabbit.flee();
+    }
+}
---
+++ src/animals/Rabbit.java
@@ -0,0 +1,7 @@
+package animals;
+
+public class Rabbit implements Prey{
+    void flee() {
+        System.out.println("running away!");
+    };
+}
